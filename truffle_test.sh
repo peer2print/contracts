dd947646036e52e881dfa9a1019b9c95f5d64dd1
n0iz <n0iz@n0iz.ddns.net>
2017-05-25 21:31:42 +0200

    log testrpc errors in file

--- truffle_test.sh
+++ truffle_test.sh
@@ -2,6 +2,6 @@
 echo "Adding local binaries in front of PATH"
 export PATH=$(npm bin):$PATH && echo "SUCCESS"
 echo "Starting testrpc"
-testrpc > testrpc.log &
+testrpc >>testrpc.log 2>>testrpc.log &
 echo "Testing"
 truffle test && echo "SUCCESS"
